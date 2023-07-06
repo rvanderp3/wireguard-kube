298827037e4db1f2f8d8a5896fc2b3a2b169418e
Richard Vanderpool <49568690+rvanderp3@users.noreply.github.com>
2023-07-06 14:42:04 -0400

    require interface be defined

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,19 +8,21 @@ THIRD_OCTET=$(echo ${HOST_IP} | cut -d'.' -f 3)
 FOURTH_OCTET=$(echo ${HOST_IP} | cut -d'.' -f 4)
 
 INDEX=$((((THIRD_OCTET%16)*256)+FOURTH_OCTET))
-
+if [ -z ${WG_INTERFACE} ]; then
+    echo "WG_INTERFACE must be defined!"
+fi
 STARTING_THIRD_OCTET=${THIRD_OCTET:-240}
 THIRD_OCTET=$(((INDEX/256)+STARTING_THIRD_OCTET))
 export WG_CLIENTIP=$(printf '172.16.%s.%s/20' ${THIRD_OCTET} ${FOURTH_OCTET})
 export WG_PRIVKEY=$(sed -n ${INDEX}p /etc/wireguard-profiles/peers)
 echo Private key index ${INDEX} client IP ${WG_CLIENTIP}
-envsubst < /etc/wireguard-profiles/wg0.conf.tmpl > /tmp/wg0.conf
-wireguard-go wg0
+envsubst < /etc/wireguard-profiles/wg0.conf.tmpl > /tmp/${WG_INTERFACE}.conf
+wireguard-go ${WG_INTERFACE}
 mkdir /etc/wireguard
-mv /tmp/wg0.conf /etc/wireguard
+mv /tmp/${WG_INTERFACE}.conf /etc/wireguard
 sleep 10
-wg-quick down wg0
-wg-quick up wg0
+wg-quick down ${WG_INTERFACE}
+wg-quick up ${WG_INTERFACE}
 
 echo Starting Wireguard on ${WG_CLIENTIP}
 
